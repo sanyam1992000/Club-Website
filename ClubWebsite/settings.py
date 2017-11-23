8fa19773b281e90524a386e8906b084682e83963
Vipin Khushu <vipinkhushu@hotmail.com>
2017-11-24 03:23:13 +0530

    features added

--- ClubWebsite/settings.py
+++ ClubWebsite/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'ao4u)1y((=+dz+taz+6ex=6hjv_tsq^c#ay@%*%12wq(v9b2*q'
 
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -82,9 +82,10 @@ WSGI_APPLICATION = 'ClubWebsite.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME':os.path.join(BASE_DIR,"db.sqlite3"),
+    }
 }
 
 
